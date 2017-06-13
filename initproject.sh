d6ec24e583ea37b9b18b9dfb1f739d9d4654f616
Kon Hyong Kim <konhyong@gmail.com>
2017-06-13 10:34:53 -0700

    added alloproject

---
+++ initproject.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git init
+
+git clone -b devel --depth 1 https://github.com/AlloSphere-Research-Group/AlloSystem.git AlloSystem
+git clone -b cmake --depth 1 https://github.com/AlloSphere-Research-Group/GLV.git GLV
+git clone -b devel --depth 1 https://github.com/AlloSphere-Research-Group/Gamma.git Gamma
+git clone --depth 1 https://github.com/rbtsx/cuttlebone.git cuttlebone
+#git submodule add https://github.com/AlloSphere-Research-Group/vsr.git AlloSystem
+
+#git add -A
+#git commit -m "Initial commit for project"
+
+# Remove remote of AlloProject to avoid committing to it
+git remote set-url --push origin no_push
+
+
