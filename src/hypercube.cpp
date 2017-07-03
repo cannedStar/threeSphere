a172af0023d457ce8945b23d4e873caed8eecfa0
Kon Hyong Kim <konhyong@gmail.com>
2017-07-03 13:32:02 -0700

    added rotation

--- src/hypercube.cpp
+++ src/hypercube.cpp
@@ -13,33 +13,82 @@
 using namespace al;
 using namespace std;
 
+static const int vertexNumber = 16;
+static const int edgeNumber = 32;
+static const int edgeResolution = 32;
+
 struct HyperApp : OmniApp {
 
   Material material;
   Light light;
 
-  Vec4f hypercube_vertices[16];
-
-// Make pointmesh and then make 3-vector.
-// Create 32 meshes for lines drawn to indicate edges of hypercube.
-
+  std::vector<Vec4f> hypercube_vertices;
+  std::vector<Vec4f> r4Edge[edgeNumber];
+  std::vector<Vec3f> s3Edge[edgeNumber];
   std::vector<Mesh> edgeMesh;
-  std::vector<Vec3f> edgePoints[32];
 
-  void generateEdge(Mesh& targetMesh, Vec4f& srcVert1, Vec4f& srcVert2, Color meshColor) {
-    targetMesh.primitive(Graphics::LINE_STRIP);
-    for (int n = 0; n <= 100; ++n) {
-      float t = (float)n / 100.f;
+  float theta;
+  float epsilon;
+  Mat4f camera;
+  Mat4f eye;
 
-      Vec3f newPoint = Vec3f(
-        (srcVert1[1]-srcVert1[1]*t+srcVert2[1]*t) / (srcVert1[0]*(-1+t)-srcVert2[0]*t + sqrt(pow((srcVert1[0]-srcVert1[0]*t+srcVert2[0]*t),2)+pow((srcVert1[1]-srcVert1[1]*t+srcVert2[1]*t),2)+pow((srcVert1[2]-srcVert1[2]*t+srcVert2[2]*t),2)+pow((srcVert1[3]-srcVert1[3]*t+srcVert2[3]*t),2))),
-        (srcVert1[2]-srcVert1[2]*t+srcVert2[2]*t) / (srcVert1[0]*(-1+t)-srcVert2[0]*t + sqrt(pow((srcVert1[0]-srcVert1[0]*t+srcVert2[0]*t),2)+pow((srcVert1[1]-srcVert1[1]*t+srcVert2[1]*t),2)+pow((srcVert1[2]-srcVert1[2]*t+srcVert2[2]*t),2)+pow((srcVert1[3]-srcVert1[3]*t+srcVert2[3]*t),2))),
-        (srcVert1[3]-srcVert1[3]*t+srcVert2[3]*t) / (srcVert1[0]*(-1+t)-srcVert2[0]*t + sqrt(pow((srcVert1[0]-srcVert1[0]*t+srcVert2[0]*t),2)+pow((srcVert1[1]-srcVert1[1]*t+srcVert2[1]*t),2)+pow((srcVert1[2]-srcVert1[2]*t+srcVert2[2]*t),2)+pow((srcVert1[3]-srcVert1[3]*t+srcVert2[3]*t),2)))
-        );                
-      targetMesh.vertex(newPoint);
-      targetMesh.color(meshColor);
+  void generateEdge(std::vector<Vec4f>& tgtEdge, Vec4f& srcVt1, Vec4f& srcVt2) {
+    
+    // tgtEdge.resize(edgeResolution);
+
+    for (int i = 0; i < edgeResolution; ++i) {
+      float t = (float)i / ((float)edgeResolution - 1.f);
+      Vec4f newPoint = srcVt1 + t * (srcVt2 - srcVt1);
+
+      // cout << newPoint << endl;
+
+      // cout << newPoint.normalized() << endl;
+
+      // projection onto S3 - change this later to other projections
+      // cout << "src " << newPoint << " -> " << newPoint.normalize() << endl;
+      // // projection onto S3 - change this later to other projections
+      // newPoint = newPoint.normalize();
+     
+      // why?????????? 
+      tgtEdge[i] = newPoint.normalized();
+    }
+
+    // cout << tgtEdge.size() << endl;
+
+    // for (int i = 0; i < edgeResolution; ++i)
+    //   cout << tgtEdge[i] << endl;
+  }
+
+  void projectR4toR3(std::vector<Vec3f>& tgtVt, std::vector<Vec4f>& srcVt) {
+    tgtVt.clear();
+
+    for (int i = 0; i < srcVt.size(); ++i) {
+
+      Vec4f postRotVt = srcVt[i];
+      Mat4f::multiply(postRotVt, camera, srcVt[i]);
+      
+      // projection onto R3
+      Vec3f newVt = Vec3f(
+        postRotVt[1] / (1.f - postRotVt[0]),
+        postRotVt[2] / (1.f - postRotVt[0]),
+        postRotVt[3] / (1.f - postRotVt[0])
+        );
+
+      tgtVt.push_back(newVt);
     }
   }
+
+  void generateMesh(Mesh& targetMesh, std::vector<Vec3f>& srcVt, HSV meshColor) {
+    targetMesh.reset();
+
+    targetMesh.primitive(Graphics::LINE_STRIP);
+    for (int i = 0; i < srcVt.size(); ++i) {
+      targetMesh.vertex(srcVt[i]);
+      targetMesh.color(meshColor);  
+    }
+  }
+
+
 
   // CONSTRUCTOR
   HyperApp() {
@@ -51,7 +100,23 @@ struct HyperApp : OmniApp {
     light.ambient(Color(1,1,1));
     initWindow();
     initAudio();
-        
+
+    theta = 0.5f;
+    camera = Mat4f(
+      cos(theta), -sin(theta), 0.f, 0.f,
+      sin(theta), cos(theta), 0.f, 0.f,
+      0.f, 0.f, cos(theta), -sin(theta),
+      0.f, 0.f, sin(theta), cos(theta));
+
+    epsilon = 0.01f;
+    eye = Mat4f(
+      cos(epsilon), 0.f, 0.f, -sin(epsilon),
+      0.f, 1.f, 0.f, 0.f,
+      0.f, 0.f, 1.f, 0.f,
+      sin(epsilon), 0.f, 0.f, cos(epsilon));
+
+    hypercube_vertices.resize(vertexNumber);
+
     // list of vertices for hypercube
     hypercube_vertices[0]  = Vec4f(-1.0,-1.0,-1.0,-1.0);
     hypercube_vertices[1]  = Vec4f(-1.0,-1.0,-1.0, 1.0);
@@ -70,42 +135,27 @@ struct HyperApp : OmniApp {
     hypercube_vertices[14] = Vec4f( 1.0, 1.0, 1.0,-1.0);
     hypercube_vertices[15] = Vec4f( 1.0, 1.0, 1.0, 1.0);
 
-    edgeMesh.resize(32);
+    for (int i = 0; i < edgeNumber; ++i)
+      r4Edge[i].resize(edgeResolution);
 
-    // Meshes
+    int k = 0;
+    for (int i = 0; i < vertexNumber; ++i) {
+      for (int j = i + 1; j < vertexNumber; ++j) {
+        Vec4f dist = (hypercube_vertices[i] - hypercube_vertices[j]) * 0.5f;
+        if (dist.mag() == 1.f) {
+          generateEdge(r4Edge[k++], hypercube_vertices[i], hypercube_vertices[j]);
+        }
+      }
+    }
 
-    generateEdge(edgeMesh[0], hypercube_vertices[0], hypercube_vertices[1], Color(0.3, 1.0, 0.3, 1.0));
-    generateEdge(edgeMesh[1], hypercube_vertices[0], hypercube_vertices[2], Color(1.0, 0.3, 0.3, 1.0));
-    generateEdge(edgeMesh[2], hypercube_vertices[0], hypercube_vertices[4], Color(1.0, 1.0, 0.0, 1.0));
-    generateEdge(edgeMesh[3], hypercube_vertices[0], hypercube_vertices[8], Color(0.0, 1.0, 1.0, 1.0));
-    generateEdge(edgeMesh[4], hypercube_vertices[1], hypercube_vertices[3], Color(0.3, 0.3, 1.0, 1.0));
-    generateEdge(edgeMesh[5], hypercube_vertices[1], hypercube_vertices[5], Color(1.0, 0.0, 1.0, 1.0));
-    generateEdge(edgeMesh[6], hypercube_vertices[1], hypercube_vertices[9], Color(1.0, 1.0, 0.0, 1.0));
-    generateEdge(edgeMesh[7], hypercube_vertices[2], hypercube_vertices[3], Color(0.3, 1.0, 0.3, 1.0));
-    generateEdge(edgeMesh[8], hypercube_vertices[2], hypercube_vertices[6], Color(0.3, 0.3, 1.0, 1.0));
-    generateEdge(edgeMesh[9], hypercube_vertices[2], hypercube_vertices[10], Color(1.0, 0.3, 0.3, 1.0));
-    generateEdge(edgeMesh[10], hypercube_vertices[3], hypercube_vertices[7], Color(0.0, 1.0, 1.0, 1.0));
-    generateEdge(edgeMesh[11], hypercube_vertices[3], hypercube_vertices[11], Color(1.0, 0.0, 1.0, 1.0));
-    generateEdge(edgeMesh[12], hypercube_vertices[4], hypercube_vertices[5], Color(0.3, 1.0, 0.3, 1.0));
-    generateEdge(edgeMesh[13], hypercube_vertices[4], hypercube_vertices[6], Color(1.0, 0.0, 1.0, 1.0));
-    generateEdge(edgeMesh[14], hypercube_vertices[4], hypercube_vertices[12], Color(0.0, 1.0, 1.0, 1.0));
-    generateEdge(edgeMesh[15], hypercube_vertices[5], hypercube_vertices[7], Color(1.0, 0.3, 0.3, 1.0));
-    generateEdge(edgeMesh[16], hypercube_vertices[5], hypercube_vertices[13], Color(0.3, 0.3, 1.0, 1.0));
-    generateEdge(edgeMesh[17], hypercube_vertices[6], hypercube_vertices[7], Color(1.0, 0.3, 0.3, 1.0));
-    generateEdge(edgeMesh[18], hypercube_vertices[6], hypercube_vertices[14], Color(0.0, 1.0, 1.0, 1.0));
-    generateEdge(edgeMesh[19], hypercube_vertices[7], hypercube_vertices[15], Color(1.0, 1.0, 0.0, 1.0));
-    generateEdge(edgeMesh[20], hypercube_vertices[8], hypercube_vertices[9], Color(0.3, 1.0, 0.3, 1.0));
-    generateEdge(edgeMesh[21], hypercube_vertices[8], hypercube_vertices[10], Color(1.0, 0.0, 1.0, 1.0));
-    generateEdge(edgeMesh[22], hypercube_vertices[8], hypercube_vertices[12], Color(0.0, 1.0, 1.0, 1.0));
-    generateEdge(edgeMesh[23], hypercube_vertices[9], hypercube_vertices[11], Color(0.3, 0.3, 1.0, 1.0));
-    generateEdge(edgeMesh[24], hypercube_vertices[9], hypercube_vertices[13], Color(0.3, 1.0, 0.3, 1.0));
-    generateEdge(edgeMesh[25], hypercube_vertices[10], hypercube_vertices[11], Color(1.0, 0.3, 0.3, 1.0));
-    generateEdge(edgeMesh[26], hypercube_vertices[10], hypercube_vertices[14], Color(1.0, 1.0, 0.0, 1.0));
-    generateEdge(edgeMesh[27], hypercube_vertices[11], hypercube_vertices[15], Color(0.0, 1.0, 1.0, 1.0));
-    generateEdge(edgeMesh[28], hypercube_vertices[12], hypercube_vertices[13], Color(0.3, 0.3, 1.0, 1.0));
-    generateEdge(edgeMesh[29], hypercube_vertices[12], hypercube_vertices[14], Color(1.0, 0.0, 1.0, 1.0));
-    generateEdge(edgeMesh[30], hypercube_vertices[13], hypercube_vertices[15], Color(1.0, 1.0, 0.0, 1.0));
-    generateEdge(edgeMesh[31], hypercube_vertices[14], hypercube_vertices[15], Color(0.3, 1.0, 0.3, 1.0));
+    cout << vertexNumber << " vertices -> " << k << " edges" << endl;
+
+    edgeMesh.resize(edgeNumber);    
+
+    for(int i = 0; i < edgeNumber; ++i) {
+      projectR4toR3(s3Edge[i], r4Edge[i]);
+      generateMesh(edgeMesh[i], s3Edge[i], HSV((float)i / (float)edgeNumber, 1.f, 1.f));
+    }
 
   } // HyperApp()
   
@@ -127,9 +177,13 @@ struct HyperApp : OmniApp {
       g.pointSize(6);
       g.lineWidth(8);
       
-      for(int i = 0; i < 32; ++i) {
+
+      for(int i = 0; i < edgeNumber; ++i) {
+        projectR4toR3(s3Edge[i], r4Edge[i]);
+        generateMesh(edgeMesh[i], s3Edge[i], HSV((float)i / (float)edgeNumber, 1.f, 1.f));
         g.draw(edgeMesh[i]);
       }
+
     g.popMatrix();
   } // onDraw
   
@@ -155,9 +209,22 @@ struct HyperApp : OmniApp {
   
   // KEYBOARD commands local
   virtual bool onKeyDown(const Keyboard& k){
-    //keydown(k.key());
-    if (k.key() == ' ') { /**/ }
+    switch (k.key()) {
+      case 'g': theta -= 0.1f; camera = Mat4f(
+      cos(theta), -sin(theta), 0.f, 0.f,
+      sin(theta), cos(theta), 0.f, 0.f,
+      0.f, 0.f, cos(theta), -sin(theta),
+      0.f, 0.f, sin(theta), cos(theta));break;
+      case 't': theta += 0.1f; camera = Mat4f(
+      cos(theta), -sin(theta), 0.f, 0.f,
+      sin(theta), cos(theta), 0.f, 0.f,
+      0.f, 0.f, cos(theta), -sin(theta),
+      0.f, 0.f, sin(theta), cos(theta));break;
+      default: break;
+    }
+
     return true;
+
   } // onKeyDown
   
   std::string fragmentCode() {
