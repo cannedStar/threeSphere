9efa36ea4edbd6816b6c13e7118b9667090b6e2f
Kon Hyong Kim <konhyong@gmail.com>
2017-10-26 13:26:30 -0700

    reduced eye seperation

--- src/hopf.cpp
+++ src/hopf.cpp
@@ -84,7 +84,7 @@ struct HyperApp : OmniApp {
     initWindow();
     initAudio();
 
-    lens().eyeSep(0.03).near(0.2).far(200); // set eyeSep to zero
+    lens().eyeSep(0.02).near(0.1).far(200); // set eyeSep to zero
 
     theta = 0.f;
     phi = 0.f;
