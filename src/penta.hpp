b63e239bee2b3f6ce2bed1a68f503d2a5c7f8886
Dennis Adderton <dennis@mat.ucsb.edu>
2017-12-21 14:14:27 -0800

    penta

---
+++ src/penta.hpp
@@ -0,0 +1,326 @@
+#ifndef INCLUDE_POLY_HPP
+#define INCLUDE_POLY_HPP
+
+#include "allocore/al_Allocore.hpp"
+
+#include <thread>
+#include <atomic>
+
+using namespace al;
+using namespace std;
+
+struct Poly {
+  int maxVert = 800;
+  int maxEdge = 1600;
+  
+  int edgeRes;
+
+  std::vector<Vec4f> verts;
+  std::vector<Vec4f> edges;
+  // std::vector<Mesh> vertMesh[2];
+  std::vector<Mesh> edgeMesh[2];
+
+  atomic<bool> dirty { false };
+  atomic<bool> busy { false };
+
+  std::thread polyThread;
+
+  void init(int er = 128) {
+    edgeRes = er;
+    dirty = false;
+    busy = false;
+
+    verts.reserve(maxVert);
+    edges.reserve(maxEdge * edgeRes);
+    // vertMesh[0].reserve(maxVert);
+    // vertMesh[1].reserve(maxVert);
+    edgeMesh[0].reserve(maxEdge);
+    edgeMesh[1].reserve(maxEdge);
+  }
+
+  void clear() {
+    verts.clear();
+    edges.clear();
+    // vertMesh[0].clear();
+    // vertMesh[1].clear();
+    edgeMesh[0].clear();
+    edgeMesh[1].clear();
+  }
+
+  void addPermutation(Vec4f seed, bool even) {
+    std::vector<Vec4f> temp;
+
+    int i0 = (seed[0] != 0)? 1 : 0;
+    int i1 = (seed[1] != 0)? 1 : 0;
+    int i2 = (seed[2] != 0)? 1 : 0;
+    int i3 = (seed[3] != 0)? 1 : 0;
+
+    for (int i = 0; i <= i0; ++i) {
+      for (int j = 0; j <= i1; ++j) {
+        for (int k = 0; k <= i2; ++k) {
+          for (int l = 0; l <= i3; ++l) {
+            float s0 = seed[0] * pow(-1, i);
+            float s1 = seed[1] * pow(-1, j);
+            float s2 = seed[2] * pow(-1, k);
+            float s3 = seed[3] * pow(-1, l);
+
+            temp.push_back(Vec4f(s0, s1, s2, s3));
+            temp.push_back(Vec4f(s0, s3, s1, s2));
+            temp.push_back(Vec4f(s0, s2, s3, s1));
+            temp.push_back(Vec4f(s1, s0, s3, s2));
+            temp.push_back(Vec4f(s1, s3, s2, s0));
+            temp.push_back(Vec4f(s1, s2, s0, s3));
+            temp.push_back(Vec4f(s2, s0, s1, s3));
+            temp.push_back(Vec4f(s2, s3, s0, s1));
+            temp.push_back(Vec4f(s2, s1, s3, s0));
+            temp.push_back(Vec4f(s3, s0, s2, s1));
+            temp.push_back(Vec4f(s3, s2, s1, s0));
+            temp.push_back(Vec4f(s3, s1, s0, s2));
+
+            if (!even) {
+              temp.push_back(Vec4f(s0, s1, s3, s2));
+              temp.push_back(Vec4f(s0, s3, s2, s1));
+              temp.push_back(Vec4f(s0, s2, s1, s3));
+              temp.push_back(Vec4f(s1, s0, s2, s3));
+              temp.push_back(Vec4f(s1, s3, s0, s2));
+              temp.push_back(Vec4f(s1, s2, s3, s0));
+              temp.push_back(Vec4f(s2, s0, s3, s1));
+              temp.push_back(Vec4f(s2, s3, s1, s0));
+              temp.push_back(Vec4f(s2, s1, s0, s3));
+              temp.push_back(Vec4f(s3, s0, s1, s2));
+              temp.push_back(Vec4f(s3, s2, s0, s1));
+              temp.push_back(Vec4f(s3, s1, s2, s0));
+            }
+          }
+        }
+      }
+    }
+
+    for (auto it = temp.begin(); it != temp.end(); ++it) {
+      for (auto it2 = std::next(it); it2 != temp.end(); ) {
+        if (*it2 == *it) {
+          it2 = temp.erase(it2);
+        }
+        else ++it2;
+      }
+    }
+
+    for (auto &i : temp) {
+      verts.push_back(i);
+    }
+  }
+
+  void generateEdge(const Vec4f& srcVt1, const Vec4f& srcVt2) {
+    for (int i = 0; i < edgeRes; ++i) {
+      float t = (float)i / ((float)edgeRes - 1.f);
+      Vec4f newPoint = srcVt1 + t * (srcVt2 - srcVt1);
+
+      // projection onto S3
+      edges.push_back(newPoint.normalized());
+    }
+  }
+
+  void generateMesh(const Mat4f& camera, const Mat4f& eye, int dualIdx) {
+    // vertMesh[0].clear();
+    // vertMesh[1].clear();
+    if (edgeMesh[0].size() != edges.size() / edgeRes) {
+      edgeMesh[0].resize(edges.size() / edgeRes);
+      edgeMesh[1].resize(edges.size() / edgeRes);
+    }
+
+    for (int i = 0; i < edgeMesh[0].size(); ++i) {
+      HSV meshColor;
+      
+      if (dualIdx == 1)
+        meshColor = HSV(0.6f, 0.7f, 0.8f);
+      else if (dualIdx == 2)
+        meshColor = HSV(0.15f, 0.8f, 1.f);
+      else
+        meshColor = HSV((float) i / (float)edgeMesh[0].size(), 1.f, 1.f);
+      
+      edgeMesh[0][i].reset();
+      edgeMesh[0][i].primitive(Graphics::LINE_STRIP);
+      edgeMesh[1][i].reset();
+      edgeMesh[1][i].primitive(Graphics::LINE_STRIP);
+
+      for (int j = 0; j < edgeRes; ++j) {
+        Vec4f left4D, right4D;
+        Mat4f::multiply(left4D, camera, edges[edgeRes * i + j]);
+        Mat4f::multiply(right4D, eye, left4D);
+
+        // projection onto R3
+        Vec3f left3D = Vec3f(
+          left4D[1] / (1.f - left4D[0]),
+          left4D[2] / (1.f - left4D[0]),
+          left4D[3] / (1.f - left4D[0])
+          );
+        Vec3f right3D = Vec3f(
+          right4D[1] / (1.f - right4D[0]),
+          right4D[2] / (1.f - right4D[0]),
+          right4D[3] / (1.f - right4D[0])
+          );
+
+        edgeMesh[0][i].vertex(left3D);
+        edgeMesh[0][i].color(meshColor);
+        edgeMesh[1][i].vertex(right3D);
+        edgeMesh[1][i].color(meshColor);
+      }
+
+      // edgeMesh[0][i].generateNormals();
+      // edgeMesh[1][i].generateNormals();
+    }
+
+    dirty = false;
+    busy = false;
+  }
+
+  void generateMeshT(const Mat4f& camera, const Mat4f& eye, const int dualIdx) {
+    busy = true;
+    polyThread = std::thread([this, &camera, &eye, dualIdx]{
+      this->generateMesh(camera, eye, dualIdx);
+    });
+    polyThread.detach();
+  }
+
+  void setHypercube() {
+    clear();
+
+    addPermutation(Vec4f(1, 1, 1, 1), false);
+
+    for (int i = 0; i < verts.size(); ++i) {
+      for (int j = i + 1; j < verts.size(); ++j) {
+        Vec4f dist = (verts[i] - verts[j]);
+        if (dist.mag() < 2.1f) {
+          generateEdge(verts[i], verts[j]);
+        }
+      }
+    }
+    
+    cout << "Hypercube: " << verts.size() << " vertices -> " << edges.size() / edgeRes << " edges" << endl;
+  }
+
+  void set5() {
+    clear();
+
+    float gold = (1.f + sqrt(5)) / 2.f;
+
+    verts.push_back(Vec4f(2, 0, 0, 0));
+    verts.push_back(Vec4f(0, 2, 0, 0));
+    verts.push_back(Vec4f(0, 0, 2, 0));
+    verts.push_back(Vec4f(0, 0, 0, 2));
+    verts.push_back(Vec4f(gold, gold, gold, gold));
+
+    // verts.push_back(Vec4f(1, 1, 1, -1/sqrt(5)));
+    // verts.push_back(Vec4f(1, -1, -1, -1/sqrt(5)));
+    // verts.push_back(Vec4f(-1, 1, -1, -1/sqrt(5)));
+    // verts.push_back(Vec4f(-1, -1, 1, -1/sqrt(5)));
+    // verts.push_back(Vec4f(0, 0, 0, sqrt(5) - 1/sqrt(5)));
+
+    // verts.push_back(Vec4f(1/sqrt(10), 1/sqrt(6), 1/sqrt(3), 1));
+    // verts.push_back(Vec4f(1/sqrt(10), 1/sqrt(6), 1/sqrt(3), -1));
+    // verts.push_back(Vec4f(1/sqrt(10), 1/sqrt(6), -2/sqrt(3), 0));
+    // verts.push_back(Vec4f(1/sqrt(10), -sqrt(3/2), 0, 0));
+    // verts.push_back(Vec4f(-2*sqrt(2/5), 0, 0, 0));
+
+    for (int i = 0; i < verts.size(); ++i) {
+      for (int j = i + 1; j < verts.size(); ++j) {
+        Vec4f dist = (verts[i] - verts[j]);
+        if (dist.mag() < 2.9f) {
+          generateEdge(verts[i], verts[j]);
+        }
+      }
+    }
+    
+    cout << "5-cell: " << verts.size() << " vertices -> " << edges.size() / edgeRes << " edges" << endl;
+  }
+
+  void set16() {
+    clear();
+
+    addPermutation(Vec4f(1, 0, 0, 0), false);
+
+    for (int i = 0; i < verts.size(); ++i) {
+      for (int j = i + 1; j < verts.size(); ++j) {
+        Vec4f dist = (verts[i] - verts[j]);
+        if (dist.mag() < 1.5f) {
+          generateEdge(verts[i], verts[j]);
+        }
+      }
+    }
+    
+    cout << "16-cell: " << verts.size() << " vertices -> " << edges.size() / edgeRes << " edges" << endl;
+  }
+
+  void set24() {
+    clear();
+
+    addPermutation(Vec4f(1, 1, 1, 1), false);
+    addPermutation(Vec4f(2, 0, 0, 0), false);
+
+    for (int i = 0; i < verts.size(); ++i) {
+      for (int j = i + 1; j < verts.size(); ++j) {
+        Vec4f dist = (verts[i] - verts[j]);
+        if (dist.mag() < 2.1f) {
+          generateEdge(verts[i], verts[j]);
+        }
+      }
+    }
+
+    cout << "24-cell: " << verts.size() << " vertices -> " << edges.size() / edgeRes << " edges" << endl;
+  }
+
+  void set120() {
+    clear();
+
+    float gold = (1.f + sqrt(5)) / 2.f;
+
+    addPermutation(Vec4f(0, 0, 2, 2), false);
+    addPermutation(Vec4f(1, 1, 1, sqrt(5)), false);
+    addPermutation(Vec4f(pow(gold, -2), gold, gold, gold), false);
+    addPermutation(Vec4f(pow(gold, -1), pow(gold, -1), pow(gold, -1), pow(gold, 2)), false);
+    addPermutation(Vec4f(0, pow(gold, -2), 1, pow(gold, 2)), true);
+    addPermutation(Vec4f(0, pow(gold, -1), gold, sqrt(5)), true);
+    addPermutation(Vec4f(pow(gold, -1), 1, gold, 2), true);
+
+    for (int i = 0; i < verts.size(); ++i) {
+      for (int j = i + 1; j < verts.size(); ++j) {
+        Vec4f dist = (verts[i] - verts[j]);
+        if (dist.mag() < 0.8f) {
+          generateEdge(verts[i], verts[j]);
+        }
+      }
+    }
+    
+    cout << "120-cell: " << verts.size() << " vertices -> " << edges.size() / edgeRes << " edges" << endl;
+  }
+
+  void set600() {
+    clear();
+
+    float gold = (1.f + sqrt(5)) / 2.f;
+
+    addPermutation(Vec4f(0.5, 0.5, 0.5, 0.5), false);
+    addPermutation(Vec4f(0, 0, 0, 1), false);
+    addPermutation(Vec4f(0.5 * gold, 0.5, 0.5 * pow(gold, -1), 0), true);
+
+    for (int i = 0; i < verts.size(); ++i) {
+      for (int j = i + 1; j < verts.size(); ++j) {
+        Vec4f dist = (verts[i] - verts[j]);
+        if (dist.mag() < 0.8f) {
+          generateEdge(verts[i], verts[j]);
+        }
+      }
+    }
+    
+    cout << "600-cell: " << verts.size() << " vertices -> " << edges.size() / edgeRes << " edges" << endl;
+  }
+
+  void draw(Graphics& g, int eye) {
+    for (int i = 0; i < edgeMesh[eye].size(); ++i) {
+      g.draw(edgeMesh[eye][i]);
+    }
+  }
+
+};
+
+#endif
---
+++ src/pentachoron.cpp
@@ -0,0 +1,264 @@
+/*
+4D Polytopes
+
+[UCSB]
+Steve Trettel
+Dennis Adderton
+Kenny Kim
+*/
+
+#include "al_OmniApp.hpp"
+#include <iostream>
+
+#include "penta.hpp"
+#include "hopf.hpp"
+
+using namespace al;
+using namespace std;
+
+struct HyperApp : OmniApp {
+
+  Material material;
+  Light light;
+
+  float theta, phi, epsilon;
+  int x_hopf, y_hopf, a_hopf, b_hopf;
+
+  Poly poly, poly2;
+  Hopf hopf;
+
+  int showState; // 0 poly 1 dual 2 hopf
+
+  Mat4f camera;
+  Mat4f eye;
+
+  // CONSTRUCTOR
+  HyperApp() {
+    nav().pos(0.0, 0.0, 0.0);
+    light.pos(0, 10.0, 0);
+    light.specular(Color(0.1,0.1,0.1));
+    light.diffuse(Color(0.2,0.2,0.2));
+    light.ambient(Color(1,1,1));
+    initWindow();
+    initAudio();
+
+    lens().eyeSep(0.02).near(0.1).far(200); // set eyeSep to zero
+
+    theta = 0.f;
+    phi = 0.f;
+    epsilon = 0.f;
+
+    x_hopf = 2;
+    y_hopf = 2;
+    a_hopf = 1;
+    b_hopf = 1;
+
+    showState = 0;
+
+    camera = Mat4f(
+      cos(theta), -sin(theta), 0.f, 0.f,
+      sin(theta), cos(theta), 0.f, 0.f,
+      0.f, 0.f, cos(theta+phi), -sin(theta+phi),
+      0.f, 0.f, sin(theta+phi), cos(theta+phi));
+
+    eye = Mat4f(
+      cos(epsilon), 0.f, 0.f, -sin(epsilon),
+      0.f, 1.f, 0.f, 0.f,
+      0.f, 0.f, 1.f, 0.f,
+      sin(epsilon), 0.f, 0.f, cos(epsilon));
+
+    poly.init();
+    poly.set16();
+
+    poly.generateMesh(camera, eye, 0);
+
+    poly2.init();
+    poly2.setHypercube();
+    poly2.generateMesh(camera, eye, 2);
+
+    hopf.init();
+    hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf);
+    hopf.generateMesh(camera, eye);
+  } // HyperApp()
+  
+  virtual ~HyperApp() {}    // what does this do?
+
+  // add in rotation functions
+
+  // ANIMATE
+  virtual void onAnimate(double dt) {
+    if(showState == 0) {
+      if(poly.dirty && !poly.busy) poly.generateMeshT(camera, eye, 0);
+    } else if (showState == 1) {
+      if(poly.dirty && !poly.busy) poly.generateMeshT(camera, eye, 1);
+      if(poly2.dirty && !poly2.busy) poly2.generateMeshT(camera, eye, 2);
+    } else if (showState == 2) {
+      if(hopf.dirty && !hopf.busy) hopf.generateMeshT(camera, eye);
+    }
+  }
+    
+  // DRAW 
+  virtual void onDraw(Graphics& g) {
+    material(); // material ...
+    light();    // light ...
+      
+    g.pushMatrix();  
+      g.depthTesting(true);
+      g.blending(true);
+      g.blendModeTrans();
+      g.pointSize(6);
+      g.lineWidth(5);
+      
+      if(showState == 0) poly.draw(g, omni().currentEye());
+      else if(showState == 1) {
+        poly.draw(g, omni().currentEye());
+        poly2.draw(g, omni().currentEye());
+      } else if(showState == 2) {
+        hopf.draw(g, omni().currentEye());
+      }
+    g.popMatrix();
+  } // onDraw
+
+  void update() {
+    if (showState == 2) {
+      camera = Mat4f(cos(theta), 0.f, 0.f, -sin(theta),
+                     0.f, cos(theta+phi), -sin(theta+phi), 0.f,
+                     0.f, sin(theta+phi), cos(theta+phi), 0.f,
+                     sin(theta), 0.f, 0.f, cos(theta));
+    } else {
+      camera = Mat4f(cos(theta), -sin(theta), 0.f, 0.f,
+                     sin(theta), cos(theta), 0.f, 0.f,
+                     0.f, 0.f, cos(theta+phi), -sin(theta+phi),
+                     0.f, 0.f, sin(theta+phi), cos(theta+phi));
+    }
+
+    eye = Mat4f(cos(epsilon), 0.f, 0.f, -sin(epsilon),
+                0.f, 1.f, 0.f, 0.f,
+                0.f, 0.f, 1.f, 0.f,
+                sin(epsilon), 0.f, 0.f, cos(epsilon));
+
+    poly.dirty = true;
+    poly2.dirty = true;
+    hopf.dirty = true;
+  }
+  
+  // MESSAGE FROM MAX
+  virtual void onMessage(osc::Message& m) {
+    std::string param;
+    float w, x, y, z;
+    int i;
+
+    if (m.addressPattern() == "/as_view_quat") {
+      m >> x >> y >> z >> w;
+      nav().quat().set(x, y, z, w);
+      nav().quat() *= Quatd().fromAxisY(M_PI);
+    } else if (m.addressPattern() == "/as_view_pos") {
+      m >> x >> y >> z;
+      nav().pos().set(x, y, z);
+    } else if (m.addressPattern() == "/as_key") {
+      m >> i; 
+      printf("OSC /as_key %d\n", i);
+      switch(i) {
+        case 'g': theta -= 0.005f; update(); break;
+        case 't': theta += 0.005f; update(); break;
+        case 'h': phi -= 0.005f; update(); break;
+        case 'y': phi += 0.005f; update(); break;
+        case '[': epsilon -= 0.01f; update(); break;
+        case ']': epsilon += 0.01f; update(); break;
+        case '\\': epsilon = 0.f; update(); break;
+        case 'r': theta = 0.f; phi = 0.f; update(); break;
+        case '1': showState = 0; poly.setHypercube(); update(); break;
+        case '2': showState = 0; poly.set16(); update(); break;
+        case '3': showState = 0; poly.set24(); update(); break;
+        case '4': showState = 0; poly.set120(); update(); break;
+        case '5': showState = 0; poly.set600(); update(); break;
+        case '6': showState = 0; poly.set5(); update(); break;
+        case '9': showState = 1; poly2.setHypercube(); poly.set16(); update(); break;
+        case '0': showState = 1; poly2.set600(); poly.set120(); update(); break;
+        case '-': showState = 2; a_hopf = 1; b_hopf = 1; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+        case '=': showState = 2; a_hopf = 2; b_hopf = 3; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+        case 'j': x_hopf++; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+        case 'm': x_hopf--; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+        case 'k': y_hopf++; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+        case ',': y_hopf--; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+        case 'l': a_hopf++; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+        case '.': a_hopf--; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+        case ';': b_hopf++; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+        case '/': b_hopf--; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+        default: break;
+      }
+    } else { m.print(); } // as_key
+  } // onMessage
+  
+  // KEYBOARD commands local
+  virtual bool onKeyDown(const Keyboard& k){
+    switch (k.key()) {
+      // case '1': omni().mode(OmniStereo::DUAL).stereo(true); break;
+      // case '2': omni().mode(OmniStereo::ANAGLYPH).stereo(true); break;
+
+      case 'g': theta -= 0.05f; update(); break;
+      case 't': theta += 0.05f; update(); break;
+      case 'h': phi -= 0.05f; update(); break;
+      case 'y': phi += 0.05f; update(); break;
+      case '[': epsilon -= 0.01f; update(); break;
+      case ']': epsilon += 0.01f; update(); break;
+      case '\\': epsilon = 0.f; update(); break;
+      case 'r': theta = 0.f; phi = 0.f; update(); break;
+      case '1': showState = 0; poly.setHypercube(); update(); break;
+      case '2': showState = 0; poly.set16(); update(); break;
+      case '3': showState = 0; poly.set24(); update(); break;
+      case '4': showState = 0; poly.set120(); update(); break;
+      case '5': showState = 0; poly.set600(); update(); break;
+      case '6': showState = 0; poly.set5(); update(); break;
+      case '9': showState = 1; poly2.setHypercube(); poly.set16(); update(); break;
+      case '0': showState = 1; poly2.set600(); poly.set120(); update(); break;
+      case '-': showState = 2; a_hopf = 1; b_hopf = 1; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+      case '=': showState = 2; a_hopf = 2; b_hopf = 3; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+      case 'j': x_hopf++; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+      case 'm': x_hopf--; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+      case 'k': y_hopf++; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+      case ',': y_hopf--; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+      case 'l': a_hopf++; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+      case '.': a_hopf--; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+      case ';': b_hopf++; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+      case '/': b_hopf--; hopf.setHopf(x_hopf, y_hopf, a_hopf, b_hopf); update(); break;
+      default: break;
+    }
+    return true;
+  } // onKeyDown
+  
+  std::string fragmentCode() {
+    return AL_STRINGIFY(
+      uniform float lighting;
+      uniform float texture;
+      uniform sampler2D texture0;
+      varying vec4 color;
+      varying vec3 normal, lightDir, eyeVec;
+
+      void main() {
+        vec4 colorMixed;
+        vec4 textureColor = texture2D(texture0, gl_TexCoord[0].st);
+        if( texture > 0.0){ colorMixed = mix(color, textureColor, texture);} 
+        else {colorMixed = color;}
+        vec4 final_color = colorMixed * gl_LightSource[0].ambient;
+        vec3 N = normalize(normal);
+        vec3 L = lightDir;
+        float lambertTerm = max(dot(N, L), 0.0);
+        final_color += gl_LightSource[0].diffuse * colorMixed * lambertTerm;
+        vec3 E = eyeVec;
+        vec3 R = reflect(-L, N);
+        float spec = pow(max(dot(R, E), 0.0), 0.9 + 1e-20);
+        final_color += gl_LightSource[0].specular * spec;
+        gl_FragColor = mix(colorMixed, final_color, lighting);
+        if (texture > 0.0) gl_FragColor.a = textureColor.r; // sets alpha to 0 for background
+      }
+    );
+  } // fragmentCode
+
+}; // struct HyperApp
+
+int main(int argc, char* argv[]) {
+  HyperApp().start();
+//  START
+  return 0;
+}
